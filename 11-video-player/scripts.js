9ed9c9f8b7eba6077b6cd63038a123a7b6f89010
Laetitia Taddei <laetdi@gmail.com>
2022-04-16 15:52:00 +0900

    fighting with CSS styling - process filled doesn't work

--- 11-video-player/scripts.js
+++ 11-video-player/scripts.js
@@ -1,12 +1,11 @@
 // get elements
-  // get the player
   const player = document.querySelector(".player");
   const video = player.querySelector(".player__video");
   const progressBar = player.querySelector(".progress");
   const progressFilled = player.querySelector(".progress__filled")
   const toggle = player.querySelector(".toggle");
   const skipButtons = player.querySelector('[data-skip');
-  const range = player.querySelector(".player__slider")
+  const range = player.querySelector(".player__slider");
 // build out functions
 
 // hook up the event listener
