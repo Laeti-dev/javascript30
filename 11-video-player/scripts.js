a39dcb82dea6c9a6a4bbc834a045091f6977f0e6
Laetitia Taddei <laetdi@gmail.com>
2022-04-16 13:47:35 +0900

    js file ant getting /l/ments needed on the plaer

---
+++ 11-video-player/scripts.js
@@ -0,0 +1,12 @@
+// get elements
+  // get the player
+  const player = document.querySelector(".player");
+  const video = player.querySelector(".player__video");
+  const progressBar = player.querySelector(".progress");
+  const progressFilled = player.querySelector(".progress__filled")
+  const toggle = player.querySelector(".toggle");
+  const skipButtons = player.querySelector('[data-skip');
+  const range = player.querySelector(".player__slider")
+// build out functions
+
+// hook up the event listener
