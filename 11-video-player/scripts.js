673911de5474489b6f431ce65a9687b674a88668
Laetitia Taddei <laetdi@gmail.com>
2022-04-19 12:47:11 +0900

    play and pause button toggle

--- 11-video-player/scripts.js
+++ 11-video-player/scripts.js
@@ -5,16 +5,24 @@
   const progressFilled = player.querySelector(".progress__filled")
   const toggle = player.querySelector(".toggle");
   const skipButtons = player.querySelector('[data-skip');
-  const range = player.querySelector(".player__slider");
+  const ranges = player.querySelector(".player__slider");
 // build out functions
 function playVideo() {
-  if (video.paused == true){
-    video.play();
-  } else {
-    video.pause();
-  }
+  // if (video.paused == true){
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
+  video.paused ? video.play() : video.pause();
 }
 
+function playButton () {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon)
+  toggle.textContent = icon
+}
 
 // hook up the event listener
-toggle.addEventListener('click', playVideo)
+video.addEventListener('click', playVideo)
+video.addEventListener('play', playButton)
+video.addEventListener('pause', playButton)
